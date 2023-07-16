dedc3d4fd80e388c6504deb809e92f8ae9dff390
Natalie Pollard <nataliepollard007@gmail.com>
2023-07-16 09:28:34 -0700

    form.vue

--- components/AddCamp/Form.vue
+++ components/AddCamp/Form.vue
@@ -1,114 +1,52 @@
 <script setup lang="ts">
-import { CAMP_ADDRESS_OPTIONS } from '../../constants/index'
+import { useMutation } from 'vue-query'
+import { CAMP_ADDRESS_OPTIONS } from '~/constants/index'
+import { useAlertStore } from '~/stores/alert'
+import { useCamps } from '~/composables/api/useCamps'
 
-const camp = ref({
-  campType: undefined,
+const { campList = [] } = defineProps<{
+  campList: Array<any>
+}>()
+
+const alertStore = useAlertStore()
+const { addLocationByCampId } = useCamps()
+
+const submitted = ref(false)
+
+function useCreateCamp() {
+  return useMutation((formSubmission: any) => addLocationByCampId(formSubmission.id, { string: formSubmission.address }),
+    {
+      onSuccess: (data: any, variables, context) => {
+        submitted.value = true
+        window.console.log('data from api', data)
+        alertStore.setAlert('Successfully Added Location', 'green')
+      },
+    })
+}
+
+const { isLoading, isError, error, isSuccess, mutate } = useCreateCamp()
+
+const campForm = ref({
+  id: '',
+  address: '',
 })
 
-// TODO: get from DB and cache
-const CAMP_OPTIONS = [
-  'Camp A',
-  'Camp B',
-  'Camp C',
-]
-
-// TODO: get from DB and cache
-const ART_OPTIONS = [
-  'Art A',
-  'Art B',
-  'Art C',
-]
-
-// TODO: Transform data to correct format for DB
-// TODO: Put command to DB
-function addCamp(fields: any) {
-  console.log(fields)
+function addCampLocation() {
+  mutate(campForm.value)
 }
+
+const CAMP_OPTIONS = campList.map((camp: any) => {
+  return {
+    label: camp.name,
+    value: camp.uid,
+  }
+})
 </script>
 
 <template>
   <div class="w-80 text-left">
-    <FormKit
-      v-model="camp"
-      type="form"
-      @submit="addCamp"
-    >
-      <p>
-        Add your camp or art
-      </p>
+    <div v-if="!isSuccess">
       <FormKit
-        id="campType"
-        type="select"
-        placeholder="Select"
-        name="campType"
-        label="Type"
-        validation="required"
-        :options="{
-          camp: 'Camp',
-          art: 'Art',
-        }"
-      />
-      <div v-if="camp.campType === 'camp'">
-        <FormKit
-          type="select"
-          placeholder="Select"
-          name="camp"
-          label="Camp"
-          validation="required"
-          :options="CAMP_OPTIONS"
-        />
-        <FormKit
-          type="select"
-          placeholder="Select"
-          name="address"
-          label="Address"
-          help="'2:00 & A' is a different from 'A & 2:00' Be careful!"
-          validation="required"
-          :options="CAMP_ADDRESS_OPTIONS"
-        />
-      </div>
-      <div v-if="camp.campType === 'art'">
-        <FormKit
-          type="select"
-          placeholder="Select"
-          name="art"
-          label="Art"
-          validation="required"
-          :options="ART_OPTIONS"
-        />
-        <!-- check if art can be located in the city. If not add error if they try to place it within those coordinates -->
-        <FormKit
-          type="number"
-          name="hour"
-          label="Hour"
-          step="1"
-          min="0"
-          max="12"
-          validation="required"
-          placeholder="Number 1-12"
-        />
-        <FormKit
-          type="number"
-          name="minute"
-          label="Minute"
-          step="1"
-          min="0"
-          max="59"
-          validation="required"
-          placeholder="Number 0-59"
-        />
-        <FormKit
-          type="number"
-          label="Distance"
-          name="distance"
-          step="1"
-          suffix="'"
-          min="0"
-          max="10000"
-          validation="required"
-        />
-      </div>
-    </FormKit>
         type="form"
         @submit="addCampLocation"
       >
@@ -139,6 +77,8 @@ function addCamp(fields: any) {
       </FormKit>
     </div>
   </div>
-  <span v-if="isError">An error occurred: {{ error }}</span>
-  <span v-else-if="isSuccess">Submitted!</span>
+  <span v-if="error">An error occurred: {{ error }}</span>
+  <span v-else-if="isSuccess">Thank you for your submission!!</span>
+  <!-- <pre>campForm: {{ campForm }}</pre>
+  <pre>CAMP_OPTIONS: {{ CAMP_OPTIONS }}</pre> -->
 </template>
