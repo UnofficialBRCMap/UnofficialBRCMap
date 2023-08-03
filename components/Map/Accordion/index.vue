d774bfc69eda3683967ab7cecbc6a8963e39b3dc
thespacemaker <me@adnan.io>
2023-08-03 10:59:32 -0500

    fix: accordian fixes

--- components/Map/Accordion/index.vue
+++ components/Map/Accordion/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import '@coreui/coreui/dist/css/coreui.min.css'
-import {
-  CAccordion,
-  CAccordionBody,
-  CAccordionHeader,
-  CAccordionItem,
-} from '@coreui/vue'
+
+import { NCollapse, NCollapseItem } from 'naive-ui'
 
 import type { LocationDictionary } from 'types/camp'
 
@@ -18,8 +14,23 @@ console.log('what have we got', props.camps)
 </script>
 
 <template>
-  <div>
-    <CAccordion>
+  <div v-for="(item, key) in props.camps" :key="key" class="my-4">
+    {{ key }}
+    <NCollapse v-for="(campArray, index) in item" :key="index" accordion class="my-1 border py-4">
+      <div>
+        <NCollapseItem :title="campArray.name" name="1">
+          <div class="p-2 text-left">
+            <h5>Camp Description</h5>
+            <p>{{ campArray.description }}</p>
+            <h5>Hometown</h5>
+            <p>{{ campArray.hometown }}</p>
+            <h5>Contact Email</h5>
+            <p>{{ campArray.contact_email }}</p>
+          </div>
+        </NCollapseItem>
+      </div>
+    </NCollapse>
+    <!-- <CAccordion>
       <CAccordionItem v-for="(item, key) in props.camps">
         {{ key }}
         <template v-for="campArray in item">
@@ -30,10 +41,15 @@ console.log('what have we got', props.camps)
               <CAccordionBody>
                 {{ campArray.description }}
               </CAccordionBody>
-            </caccordionheader>
+            </CAccordionHeader>
           </CAccordionItem>
         </template>
+        <NCollapse v-for="(campArray, index) in item" :key="index" default-expanded-names="1" accordion>
+          <NCollapseItem title="right" name="1">
+            <div>good</div>
+          </NCollapseItem>
+        </NCollapse>
       </CAccordionItem>
-    </CAccordion>
+    </CAccordion> -->
   </div>
 </template>
